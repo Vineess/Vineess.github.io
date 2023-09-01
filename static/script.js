b35727d11b22885fdede752e195358b0a7da1f87
vinicius Soares <vinesoares1@gmail.com>
2023-09-01 19:30:24 -0300

    projeto pronto

---
+++ static/script.js
@@ -0,0 +1,61 @@
+/*ABRE E FECHA MENU LATERAL EM MODO MOBILE*/
+
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+});
+
+/*FECHA O MENU QUANDO CLICAR ALGUNS ITEM E MUDA O MENU PRA LIST*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item =>{
+    item.addEventListener("click", () =>{
+        if (body.classList.contains("menu-nav-active")) {
+          body.classList.remove("menu-nav-active") 
+           menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
+
+//ANIMAR TODOS O ITENS DA TELA QUE TIVEREM O ATRIBUTO DATA-ANIME
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85
+    item.forEach(Element =>{
+        if(windowTop > Element.offsetTop){
+            Element.classList.add("animate");
+        }else{
+            Element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
+
+//ATIVAR O CARREGAMENTO DO BOTAO ENVIAR
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener('click', () =>{
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
+})
+
+//TIRAR A MENSAGEM DEPOIS DE 5 SEGUNDOS
+
+setTimeout(() =>{
+    document.querySelector('#alerta').style.display ='none';
+}, 5000)
